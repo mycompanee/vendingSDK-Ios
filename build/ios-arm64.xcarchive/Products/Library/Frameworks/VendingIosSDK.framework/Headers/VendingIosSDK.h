7bc8968df428b8eee67ce619ea57f24e3134acd4
Steffen Brauner <steffen.brauer@hellotess.com>
2025-12-02 11:23:33 +0100

    Revert "Delete build/ios-arm64.xcarchive directory"
    This reverts commit 6b2ebc646c33fb02b24c4c8f3351782a76ac27ca.

---
+++ build/ios-arm64.xcarchive/Products/Library/Frameworks/VendingIosSDK.framework/Headers/VendingIosSDK.h
@@ -0,0 +1,23 @@
+//
+//  VendingIosSDK.h
+//  VendingIosSDK
+//
+//  Created on $(DATE).
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for VendingIosSDK.
+FOUNDATION_EXPORT double VendingIosSDKVersionNumber;
+
+//! Project version string for VendingIosSDK.
+FOUNDATION_EXPORT const unsigned char VendingIosSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VendingIosSDK/PublicHeader.h>
+
+#import <VendingIosSDK/Config.h>
+
+#if __has_include(<VendingIosSDK/VendingIosSDK-Swift.h>)
+#import <VendingIosSDK/VendingIosSDK-Swift.h>
+#endif
+
